e309a7ba01940e5ffed87533150862d983884769
Klaudia Augustyńska <klaudynka@mat.umk.pl>
2016-09-09 11:36:24 +0200

    data access layer

---
+++ TodoList/DAL/TodoListContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.DAL
+{
+    public class TodoListContext : DbContext
+    {
+        public TodoListContext() : base("TodoListContext")
+        {
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ TodoList/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public class Task
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool Completed { get; set; }
+    }
+}
